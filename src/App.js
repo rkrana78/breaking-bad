ca4ae955f041f16a81361ed85df3e9d85db63286
rkrana78 <rashedul.karim7858@gmail.com>
2021-12-07 21:29:40 +0600

    creating breaking bad character intro

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { useEffect, useState } from 'react';
+import './App.css';
+import Character from './components/Character/Character';
+import Header from './components/Header/Header';
+import Search from './components/Header/Search';
+
+const  App = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState('')
+
+  useEffect(() => {
+    const url = `https://www.breakingbadapi.com/api/characters?name=${query}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      setItems(data)
+      setIsLoading(false)
+    })
+  }, [query])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header/>
+      <Search getQuery = {(q) => setQuery(q)}/>
+      <Character isLoading={isLoading} items={items}/>
     </div>
   );
 }
